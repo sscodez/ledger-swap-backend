d617219b35785036b1f43c8a8afdf073fa05911d
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-09-22 03:56:08 +0500

    added

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -3,54 +3,64 @@ import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
 import User from '../models/User';
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID!,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-  callbackURL: '/api/auth/google/callback',
-},
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ googleId: profile.id });
+// Only configure Google OAuth if credentials are provided
+if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/api/auth/google/callback',
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ googleId: profile.id });
 
-    if (!user) {
-      user = await User.create({
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails?.[0].value,
-        profilePicture: profile.photos?.[0].value,
-      });
+      if (!user) {
+        user = await User.create({
+          googleId: profile.id,
+          name: profile.displayName,
+          email: profile.emails?.[0].value,
+          profilePicture: profile.photos?.[0].value,
+        });
+      }
+
+      done(null, user);
+    } catch (error) {
+      done(error, false);
     }
+  }));
+} else {
+  console.warn('Google OAuth not configured - missing GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET');
+}
 
-    done(null, user);
-  } catch (error) {
-    done(error, false);
-  }
-}));
+// Only configure Facebook OAuth if credentials are provided
+if (process.env.FACEBOOK_APP_ID && process.env.FACEBOOK_APP_SECRET) {
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: '/api/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'emails', 'picture.type(large)'],
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ facebookId: profile.id });
 
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID!,
-  clientSecret: process.env.FACEBOOK_APP_SECRET!,
-  callbackURL: '/api/auth/facebook/callback',
-  profileFields: ['id', 'displayName', 'emails', 'picture.type(large)'],
-},
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ facebookId: profile.id });
+      if (!user) {
+        user = await User.create({
+          facebookId: profile.id,
+          name: profile.displayName,
+          email: profile.emails?.[0].value,
+          profilePicture: profile.photos?.[0].value,
+        });
+      }
 
-    if (!user) {
-      user = await User.create({
-        facebookId: profile.id,
-        name: profile.displayName,
-        email: profile.emails?.[0].value,
-        profilePicture: profile.photos?.[0].value,
-      });
+      done(null, user);
+    } catch (error) {
+      done(error, false);
     }
-
-    done(null, user);
-  } catch (error) {
-    done(error, false);
-  }
-}));
+  }));
+} else {
+  console.warn('Facebook OAuth not configured - missing FACEBOOK_APP_ID or FACEBOOK_APP_SECRET');
+}
 
 passport.serializeUser((user: any, done) => {
   done(null, user.id);
